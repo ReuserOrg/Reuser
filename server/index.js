56e07679eae924bc17db8dae1ef9c20269871b40
Marcus Crane <marcus@thingsima.de>
2016-08-17 20:00:12 +1200

    Migrated from colors to chalk

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const path = require('path')
 const colors = require('colors')
+const chalk = require('chalk')
+
+const gray = chalk.gray
+const yellow = chalk.yellow
+const green = chalk.green
+const cyan = chalk.cyan
 
 const app = express()
 const port = process.env.PORT || 4200
 app.use(express.static(path.join(__dirname, '/../public')))
 
 console.log(
-'********************************************\n'.grey +
-'  Server status: '.yellow + 'Online\n'.green +
-'********************************************\n'.grey +
-'  Reuser is listening on port:'.yellow + port + '\n' +
-'********************************************\n'.grey +
-'  Cleaning the world...\n'.cyan +
-'  One item at a time!\n'.cyan +
-'********************************************'.grey)
+gray('********************************************\n') +
+yellow('  Server status: ') + green('Online\n') +
+gray('********************************************\n') +
+yellow(`  Reuser is listening on port: ${chalk.white(port)} \n`) +
+gray('********************************************\n') +
+cyan('  Cleaning the world...\n') +
+cyan('  One item at a time!\n') +
+gray('********************************************'))
 
 app.listen(port)
