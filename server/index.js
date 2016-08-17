c35c4f971f18efbe1bf3ee20ccef8ba7a8b30990
Marcus Crane <marcus@thingsima.de>
2016-08-17 19:52:36 +1200

    Made server log output one line and easier to read

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const app = express()
 const port = process.env.PORT || 4200
 app.use(express.static(path.join(__dirname, '/../public')))
 
-console.log('********************************************'.grey)
-console.log('  Server status: '.yellow + 'Online'.green)
-console.log('********************************************'.grey)
-console.log('  Reuser is listening on port: '.yellow + port)
-console.log('********************************************'.grey)
-console.log('  Cleaning the world...'.cyan)
-console.log('  One item at a time!'.cyan)
-console.log('********************************************'.grey)
+console.log(
+'********************************************\n'.grey +
+'  Server status: '.yellow + 'Online\n'.green +
+'********************************************\n'.grey +
+'  Reuser is listening on port:'.yellow + port + '\n' +
+'********************************************\n'.grey +
+'  Cleaning the world...\n'.cyan +
+'  One item at a time!\n'.cyan +
+'********************************************'.grey)
+
 app.listen(port)
